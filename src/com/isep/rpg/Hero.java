aca414452669e6a6afe4f7208a725bb731be2357
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-11 12:12:21 +0100

    maj 0.1

--- src/com/isep/rpg/Hero.java
+++ src/com/isep/rpg/Hero.java
@@ -1,18 +1,27 @@
 package com.isep.rpg;
 
-import java.util.Arrays;
 import java.util.List;
 
 public abstract class Hero {
     public int lP;
     public int armor;
     public int weaponDamage;
-    private List potions;
-    private List lembas;
+    public List potions;
+    public List lembas;
     SpellCaster gandalf = new Mage(100, 100, 60, 30);
 
-    public static void guérrisseur() {
+    public static void guerrisseur() {
         SpellCaster soraka = new Healer(100, 100, 30, 15);
         System.out.println(soraka);
     }
+    public static void attack(){
+
+    }
+    public static void defend(){
+
+    }
+    /*public static void useConsumable(Consumable){
+
+    }
+    */
 }
--- src/com/isep/rpg/Potion.java
+++ src/com/isep/rpg/Potion.java
@@ -1,4 +1,10 @@
 package com.isep.rpg;
 
 public class Potion {
+    public void healPotion(){
+        lP += 50;
+    }
+    public void manaPotion(){
+        mP += 20
+    }
 }
