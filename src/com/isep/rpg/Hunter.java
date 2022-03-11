07af1f941e03371349dc716f25411397dd45777e
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-11 11:56:01 +0100

    hunter&warrior

--- src/com/isep/rpg/Hunter.java
+++ src/com/isep/rpg/Hunter.java
@@ -1,5 +1,11 @@
 package com.isep.rpg;
 
 public class Hunter extends Hero{
-    int arrows = 30;
+    int arrows;
+    public Hunter(int i, int i1, int i2, int i3){
+        this.lP = i;
+        this.arrows = i1;
+        this.armor = i2;
+        this.weaponDamage = i3;
+    }
 }
