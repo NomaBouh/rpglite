163e310fc780bc8740b7e0f3f6b7a621e9dfe4f7
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-13 20:31:52 +0100

    maj 0.1.6

--- src/com/isep/rpg/Game.java
+++ src/com/isep/rpg/Game.java
@@ -7,6 +7,18 @@ public class Game {
     public int playerTurn;
 
     public static void playGame(){
+        //Initialiser le nombre de personnage jouables
+        //Définir les classes des personnage choisie si nb max non atteint
+
+        //Démarer partie (définir le nombre de tour en fonction du nombre de PJ
+
+        //Système d'upgrade de stat après chaques combats
+
+        //Deafait
+
+        //Gestion du système d'inventaire à l'aide d'une list par personnages
+
+        //Gestion de système de lancer de mana/dégat/défense
 
     }
     public static void generateCombat(){
