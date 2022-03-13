47c6fcc900ab177c3e1e19bd39b90c67b61e8229
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-13 23:02:09 +0100

    maj 0.1.9

--- src/com/isep/rpg/Game.java
+++ src/com/isep/rpg/Game.java
@@ -1,16 +1,25 @@
 package com.isep.rpg;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Game {
     public List <Hero>players = new LinkedList<>();
     public int playerTurn;
 
-    public static void classSelect(int nbChara){
+    public static int[] classSelect(int nbChara) {
+        Scanner scanner = new Scanner(System.in);
 
+        int[] persos = {};
+        for (int i = nbChara; i > 0; i--) {
+            System.out.println("Sélectionner les Compagnons De Victimus!");
+            System.out.println("1.Gandalf - Mage\n2.Soraka - Healer\n3.Geralt - Chasseur\n4.Gutz - Guérrier");
+            int select = scanner.nextInt();
+            persos[nbChara]=select;
+            }
+        System.out.println("Heureusement pour vous, Et pour VICTIMUS,\nVous pouviez choisir plusieurs fois le même personnage\nCar VICTIMUS peut faire du clonnage\nA l'aide de son MagnumMordificus!\n(... Et ne demander ce que c'est!)");
+        return persos;
     }
+
 
     public static void playGame(){
         //Initialiser le nombre de personnage jouables
@@ -24,8 +33,12 @@ public class Game {
             System.out.println("VICTIMUS n'as guère besoin d'une si grande companie\n (... Car VICTIMUS n'as plus un crésus!)");
         } else if(nbCompagnons == 1){
             System.out.println("VICTIMUS se demande lequel de ses compagnons l'accompagneras!\n (... En espérant qu'il ne coutera pas trop chère à VICTIMUS!)");
+            String tabula = Arrays.toString(new int[][]{classSelect(nbCompagnons)});
+            System.out.println(tabula);
         } else {
             System.out.println("VICTIMUS se demande lesquels de ses compagnons l'accompagnerons!\n (En espérant qu'il ne lui couterons pas un Round!)");
+            String tabula = Arrays.toString(new int[][]{classSelect(nbCompagnons)});
+            System.out.println(tabula);
         }
 
 
