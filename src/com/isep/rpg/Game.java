2677eb6a942ede2cc7e4ec58f3799ad156678cb7
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-13 20:26:58 +0100

    maj 0.1.5

--- src/com/isep/rpg/Game.java
+++ src/com/isep/rpg/Game.java
@@ -3,8 +3,8 @@ package com.isep.rpg;
 import java.util.LinkedList;
 import java.util.List;
 public class Game {
-    private List <Hero>players = new LinkedList<>();
-    private int playerTurn;
+    public List <Hero>players = new LinkedList<>();
+    public int playerTurn;
 
     public static void playGame(){
 
