8039e7440d4da427c65f6093ceed9c5b65abd5b8
NomaBouh <thbo62126@eleve.isep.fr>
2022-03-13 22:17:01 +0100

    maj 0.1.8

--- src/com/isep/rpg/Game.java
+++ src/com/isep/rpg/Game.java
@@ -2,13 +2,33 @@ package com.isep.rpg;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
+
 public class Game {
     public List <Hero>players = new LinkedList<>();
     public int playerTurn;
 
+    public static void classSelect(int nbChara){
+
+    }
+
     public static void playGame(){
         //Initialiser le nombre de personnage jouables
-        System.out.println("Bienvenue sur RAT, POGNOS ET VICTIMUS ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenue sur RAT, POGNONS ET VICTIMUS");
+        System.out.println("Selectionner le nombre de compagnions de VICTIMUS :");
+        int nbCompagnons = scanner.nextInt();
+        if(nbCompagnons == 4){
+            System.out.println("VICTIMUS sera donc accompagné de tous ses accolytes!\n En Avant!");
+        } else if(nbCompagnons>4) {
+            System.out.println("VICTIMUS n'as guère besoin d'une si grande companie\n (... Car VICTIMUS n'as plus un crésus!)");
+        } else if(nbCompagnons == 1){
+            System.out.println("VICTIMUS se demande lequel de ses compagnons l'accompagneras!\n (... En espérant qu'il ne coutera pas trop chère à VICTIMUS!)");
+        } else {
+            System.out.println("VICTIMUS se demande lesquels de ses compagnons l'accompagnerons!\n (En espérant qu'il ne lui couterons pas un Round!)");
+        }
+
+
         //Définir les classes des personnage choisie si nb max non atteint
 
         //Démarer partie (définir le nombre de tour en fonction du nombre de PJ
--- src/com/isep/rpg/Main.java
+++ src/com/isep/rpg/Main.java
@@ -3,5 +3,6 @@ package com.isep.rpg;
 public class Main {
     public static void main(String[] args) {
         com.isep.rpg.Hero.guerrisseur();
+        com.isep.rpg.Game.playGame();
     }
 }
